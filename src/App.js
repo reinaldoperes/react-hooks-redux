8a30c24de109c344e84d1ced671dd35875757442
reinaldoperes <reinaldoporte@hotmail.com>
2020-10-19 13:35:40 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import store from './store';
+
+import CourseList from './components/CourseList';
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+        <CourseList />
+      </div>
+    </Provider>
+  );
+}
+
+export default App;
